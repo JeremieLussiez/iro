f99405cd4741e76be3107e79d6e0525c14c633e3
Jérémie Lussiez <jeremie.lussiez@gmail.com>
2017-11-02 23:48:58 +0100

    feature: lerp function

--- device/client/application/components/iro/iro.js
+++ device/client/application/components/iro/iro.js
@@ -8,6 +8,31 @@ import en from "./labels/en";
 let translatorInstance = new Translator();
 translatorInstance.addLanguageLabels(fr, en);
 
+function lerp(source, start, target, end, time) {
+    return source + (target - source) * (time - start) * (1.0 / (end - start));
+}
+
+function lerpColor(source, start, target, end, time) {
+    return {
+        r: Math.round(lerp(source.r, start, target.r, end, time)),
+        g: Math.round(lerp(source.g, start, target.g, end, time)),
+        b: Math.round(lerp(source.b, start, target.b, end, time))
+    }
+}
+
+function htmlColorToRGB(htmlColor) {
+    const value = parseInt("0x" + htmlColor.substring(1, 7));
+    return {
+        r: (value >> 16) & 0xFF,
+        g: (value >> 8) & 0xFF,
+        b: value & 0xFF
+    }
+}
+
+function rgbToHTMLColor(rgbColor) {
+    return `#${Number(rgbColor.r).toString(16)}${Number(rgbColor.g).toString(16)}${Number(rgbColor.b).toString(16)}`;
+}
+
 let component = Vue.component("iro-iro", {
     props: ["title", "definition"],
     template: template,
@@ -67,6 +92,45 @@ let component = Vue.component("iro-iro", {
             led.x = Math.cos(Math.PI * 2 / this.leds.length * i) * this.ringRadius + this.ledRadius + this.ringRadius;
             led.y = Math.sin(Math.PI * 2 / this.leds.length * i) * this.ringRadius + this.ledRadius + this.ringRadius;
         }
+
+        const rainbow = [
+            "#2196f3",
+            "#3f51b5",
+            "#9c27b0",
+            "#ff5722",
+            "#ffeb3b",
+            "#4baf4f"
+        ];
+
+        /*const rainbow = [
+            "#e91e63",
+            "#ffeb3b",
+        ];*/
+
+        let ledIndex = 0;
+        const steps = this.leds.length / rainbow.length;
+        for (let i = 0; i < rainbow.length - 1; i++) {
+            for (let j = 0; j < steps; j++) {
+                this.leds[ledIndex].color = rgbToHTMLColor(lerpColor(htmlColorToRGB(rainbow[i]), 0, htmlColorToRGB(rainbow[i + 1]), steps, j));
+                ledIndex++;
+            }
+        }
+        for (let j = 0; j < steps; j++) {
+            this.leds[ledIndex].color = rgbToHTMLColor(lerpColor(htmlColorToRGB(rainbow[rainbow.length - 1]), 0, htmlColorToRGB(rainbow[0]), steps, j));
+            ledIndex++;
+        }
+
+        /* for (let i = 0; i < this.leds.length; i++) {
+             const led = this.leds[i];
+             led.color = "#eeeeee";
+         }
+
+         const trailSize = 12;
+         for (let i = 0; i <trailSize; i++) {
+             this.leds[i].color = rgbToHTMLColor(lerpColor(htmlColorToRGB("#2196f3"), 0, htmlColorToRGB("#eeeeee"), trailSize, i));
+         }*/
+
+
         this.timer = window.setInterval(() => {
             const firstLedColor = this.leds[0].color;
             for (let i = 0; i < this.leds.length - 1; i++) {
