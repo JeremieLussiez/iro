c8fbcdcbf81edece55ec28c1c1f7c5e36ebdd960
Jérémie Lussiez <jluissiez@kiloutou.fr>
2018-04-19 20:39:27 +0200

    feature: server scripts

--- configure-server.sh
+++ configure-server.sh
@@ -2,7 +2,6 @@
 apt-get update && apt-get upgrade -y
 apt-get install -y curl
 curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -
-curl -sL https://deb.nodesource.com/setup_9.x | sudo bash -
 apt-get install -y build-essential
 apt-get install -y mongodb
 apt-get install -y git
